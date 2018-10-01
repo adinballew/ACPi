51a45ba865e95cca983d4188af04c102c4249202
adinballew <adinballew@gmail.com>
2018-09-30 23:19:14 -0500

    Simple Comma Change

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,7 +16,7 @@ const chart = new Chart(ctx, {
             backgroundColor: '#14db74',
             pointRadius: 5,
             pointHoverRadius: 7,
-            lineTension: 0,
+            lineTension: 0
         }, {
             label: "Humidity",
             borderColor: "#4152ff",
@@ -26,7 +26,7 @@ const chart = new Chart(ctx, {
             backgroundColor: '#8330ff',
             pointRadius: 5,
             pointHoverRadius: 7,
-            lineTension: 0,
+            lineTension: 0
         }]
     },
     // Configuration options go here
